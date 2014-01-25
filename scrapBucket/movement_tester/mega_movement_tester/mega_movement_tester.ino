112ede3a7c91bc433b774294de02061560949fd1
vptarmigan <vicky.ptarmigan@gmail.com>
2014-01-25 13:50:06 -0600

    hi level skeleton to test move + gap finding

--- scrapBucket/movement_tester/mega_movement_tester/mega_movement_tester.ino
+++ scrapBucket/movement_tester/mega_movement_tester/mega_movement_tester.ino
@@ -1,17 +1,22 @@
-
-
-
-/* gap finder tester 
+/* mega movement tester
  * written by: victoria wu
  * date: 1/17/14 
  *
  * what: make the robot keep going until the gap is found. then stop. (going to put it all on the mega, no bridge just yet
- * FIRST - just getting the robot to move. 
+ * FIRST - just getting the robot to move. NO comms between mini and mega. just everything on the mega, ignoring encoders right now
  * PURPOSE: 
  
  * .... pro tip: if you get the error "serial2 is not declared in this scope" check that the board you're programming for IS THE MEGA
+ * -> first starting. .the motors just go crazy  a bit 
  */
 
+#include <AnalogDistanceSensor.h>
+#include <Distance2D120X.h>
+#include <DistanceSensor.h>
+
+#include <gapfinder.h>
+#include <irsensor_tester.h>
+#include <magellan_edgesensors.h>
 
 #include <motor_cmd.h>
 #include <wesley_mega_bridge.h>
@@ -20,98 +25,55 @@
 #define console Serial
 
 
-
-mega_bridge mini_br;
 motor_cmd sabertooth;
-//Encoder myEnc(A10, A11);
-//long oldPosition  = -999;
+GapFinder gapfind;
+
+
+enum state { start, moving, stopped };
+state current_status;
 
 void setup() {
 	console.begin(9600);
-	sabertooth.begin(2);
-	mini_br.begin(3);
+	sabertooth.begin(2);        //motor controller on serial2
 	console.println("ready");
+        gapfind.init(A0,A1,A2);
+        current_status = start;
 }
 
 void loop() {
-	/* comment out for testing */
-
-       
-       
-      /*
-	if (console.available() > 0) {
-		// process incoming commands from console
-		const byte cmd = console.read();
-		switch(cmd) {
-			case 'w':
-				console.println("forward");
-				sabertooth.forward(40);
-			//	sabertooth.forward();
-				break;
-			case 's':
-				console.println("reverse");
-				sabertooth.reverse(40);
-			//	sabertooth.reverse();
-				break;
-			case 'x':
-			default:
-				console.println("allstop");
-				sabertooth.all_stop();
-				break;
-
-		}
-
-	}	
-*/
-
-
-        //one problem. since the sabertooth assumes starting as stopped, you can never go forward becasue it checks if the direction is forward.
-        if (mini_br.cmd_waiting() ) {
-                //sabertooth.DIRECTION != sabertooth.STOPPED) {
-                console.print("cmd from mini: ");
-                console.println();
-                byte cmd = mini_br.cmd();
-                console.println(cmd,DEC);
-                delay(6);
-                switch(cmd) {
-                        case '-':  //speed command
-                                console.println("Speed Command!");
-                                delay(10);
-                                cmd = mini_br.cmd();
-                                console.println(cmd);
-                                //sabertooth.forward(cmd);
-                                
-                                //if (sabertooth.DIRECTION == sabertooth.FORWARD) {
-                                 //       console.print("go forwards");
-                                  //      sabertooth.forward(mini_br.cmd());
-                               // } else
-                                //if (sabertooth.DIRECTION == sabertooth.REVERSE) {
-                                //        console.print("go backwards");
-                                 //       sabertooth.reverse(mini_br.cmd());
-                                //}
-                                
-                                //console.print(mini_br.cmd(), HEX);
-                                
-                                break;
-                        default:
-                                console.print("what is this?");
-                                console.print(cmd,DEC);
-                                //sabertooth.all_stop();   
-                                break;
-                }
-                console.println();
-                delay(100);
-        }        
-       
         
+        //keep going forward until you find gap
+        gapfind.update();  
+        gapfind.printGapStatus();
+        gapfind.printDebug();
+        switch (current_status) {
+          case start:
+            //let's keep going
+            console.println("start \t go forward!");
+            sabertooth.reverse(20);
+            current_status = moving;
+            break;
+          case moving:
+            console.println("moving \t checking gap");
+            //check if a gap has been found
+            if(gapfind.gapPresent())  {
+              console.println("moving \t GAP FOUND!!");
+              current_status = stopped;
+            }
+            break;
+          case stopped:
+            console.println("stopped \t ");
+            //stay here forever
+            sabertooth.all_stop();
+            break;
+            
         
-	//sabertooth.forward(40);
-        
-        /*
-        long newPosition = myEnc.read();
-        if (newPosition != oldPosition) {
-            oldPosition = newPosition;
-            Serial.println(newPosition);
         }
-        */
+        
+        
+        delay(100);  //make it readable
+        //gapfind.printDebug();
+        
+       
+     
 }
