6c0f3e2e174b7a17320114f5efe70c9bd3655954
vptarmigan <vicky.ptarmigan@gmail.com>
2014-01-27 16:07:38 -0600

    tester code for shiny new encoder class

--- scrapBucket/QuadEncoderTest3/QuadEncoderTest3.ino
+++ scrapBucket/QuadEncoderTest3/QuadEncoderTest3.ino
@@ -1,150 +1,38 @@
+#include <motor_cmd.h>
+#include <QuadEncoder.h>
+
 /*Encoder stuff
 * written by Darren Cogan
 * UMKC robotics 2014
-/
-
-// Theoretical maximum speed of motor is 7840 CPS
-// This means 0.1276 ms per count. Or 2040 computer cycles per count!
-
-// Usable PCINT Ports on Arduino Mega:
-//   PORTB: PCINT[7:0]	  MegaPINS: 13:10, 50:53    Interrupt: PCI0
-//   PORTK: PCINT[23:16]  MegaPINS: Analog 15:8     Interrupt: PCI2 
-
-// Assuming it is wired up in this configuration
-// FR    BR    FL    BL
-// 13:12 11:10 50:51 52:53
-// A15   A13   A11   A9
-
-// We will use PORTB
-
-// Stuff to keep track of
-volatile static uint8_t state;
-volatile static uint8_t prev_state;
-volatile static int32_t positionFR;
-volatile static int32_t positionBR;
-volatile static int32_t positionFL;
-volatile static int32_t positionBL;
+*/
+QuadEncoder encoders;
+motor_cmd sabertooth;
 
 
 void setup() {
 
-	// Set all the port pins as INPUT
-	// DDRx is the Data Direction Register for PORTx
-	DDRK = 0x00;
-
-	//??? Do our encoders need pull-up? NO
-        PORTK = 0x00;
-
-	// PCMSKx registers control which pins contribute to the interrupt
-	// Set bit activates interrupt. x corresponds to PCIx
-	// For our purposes we actually want to use the entire port
-	PCMSK2 = 0xFF;
-
-	// Now lets enable the interrupt
-	PCICR |= B00000100; //last 3 bits enable the PCI2, PCI1, and PCI0 interrupts respectively
-
-	//initialize state
-	state = PINK;
-	prev_state = state;
-
-        //Activate Interrupts in general
-        sei();
-
-  
+  encoders.init();
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial.begin(9600);
+  sabertooth.begin(2);      
 }
 
 void loop() {
   // put your main code here, to run repeatedly: 
+  sabertooth.turn_right(360);
+  
   Serial.print("FL: ");
-  Serial.print(positionFL);
+  Serial.print(encoders.getTicksFR());
+  /*
   Serial.print(" BL: ");
   Serial.print(positionBL);
   Serial.print(" FR: ");
   Serial.print(positionFR);
   Serial.print(" BR: ");
   Serial.println(positionBR);
-  digitalWrite(13, LOW);
-  Serial.println(PINK);
+  digitalWrite(13, LOW);  //whats this for? (TODO ask darren)
+  Serial.println(PINK);    //whats this for?
+  */
 }
 
 
-// The ISR macro lets us actually define the code in our Interrupt Service Routine
-// PCINT0_vect should point to memory address 0x0012 on the Atmega2560
-ISR(PCINT2_vect)
-{
-	// encoder code goes here
-	char tmp1;
-
-	//get current state
-	state = PINK;
-
-        //Shuffle states
-        tmp1 = (prev_state & B00110011) | ((state & B00110011) << 2);
-
-	//compare them and update count
-	switch (tmp1 & B00001111) {
-		case 1: case 7: case 8: case 14:
-			positionBL++;
-			break;
-		case 2: case 4: case 11: case 13:
-			positionBL--;
-			break;
-		case 3: case 12:
-			positionBL += 2;
-			break;
-		case 6: case 9:
-			positionBL -= 2;
-			break;
-	}
-
-	switch (tmp1 >> 4) {
-		case 1: case 7: case 8: case 14:
-			positionBR++;
-			break;
-		case 2: case 4: case 11: case 13:
-			positionBR--;
-			break;
-		case 3: case 12:
-			positionBR += 2;
-			break;
-		case 6: case 9:
-			positionBR -= 2;
-			break;
-                default:
-                        break;
-	}
-
-        tmp1 = ((prev_state & B11001100) >> 2) | (state & B11001100);
-	switch (tmp1 & B00001111) {
-		case 1: case 7: case 8: case 14:
-			positionFL++;
-			break;
-		case 2: case 4: case 11: case 13:
-			positionFL--;
-			break;
-		case 3: case 12:
-			positionFL += 2;
-			break;
-		case 6: case 9:
-			positionFL -= 2;
-			break;
-	}
-	
-	switch (tmp1 >> 4) {
-		case 1: case 7: case 8: case 14:
-			positionFR++;
-			break;
-		case 2: case 4: case 11: case 13:
-			positionFR--;
-			break;
-		case 3: case 12:
-			positionFR += 2;
-			break;
-		case 6: case 9:
-			positionFR -= 2;
-			break;
-	}
-        prev_state = state;
-}
