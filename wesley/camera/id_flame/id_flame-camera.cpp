1453244dd9686d707928b6de5a96912ab3c81d71
Eric M Gonzalez <eric.gonzalez@umkc.edu>
2014-02-17 19:17:56 -0600

    id_tool-camera.cpp - identify a tool in a viewport
    all this code does is look through the camera and a small ROI in an
    attempt to identify a tool. it is up to the caller to put the arm in
    the correct position.
    
    as it stands, id_flame-camera and id_tool-camera both leave windows
    open so that the user can see the end result.

--- wesley/camera/id_flame/id_flame-camera.cpp
+++ wesley/camera/id_flame/id_flame-camera.cpp
@@ -50,9 +50,9 @@
  * 			on 1, 2, or 3 - the caller should pass the identified rig
  * 			to the tool identification program.
  *
- * 			on 10 or greater - these return values indicate failure. the
- * 			caller should identify the failure, display an LED array code,
- * 			and write a descriptive error to a log on disk.
+ * 			on > 10 - these return values indicate failure. the caller
+ * 			should identify the failure, display an LED array code, and
+ * 			write a descriptive error to a log on disk.
  */
 
 #include <cv.h>			// base opencv library
@@ -64,105 +64,104 @@ using namespace cv;
 #include <sys/stat.h>	// stat() - file presence
 
 int main(int argc, char* argv[]) {
-	// for readability of code
+	// in production code, this program will have no arguments.
+//	if (argc != 2) {
+//		std::cerr << argv[0] << " <frame to test>" << std::endl;
+//		return(50);
+//	}
+
 	enum rig_shapes { NONE, SQUARE, TRIANGLE, CIRCLE };
 	short matched_rig = NONE;
 
-	// create a capture frame of the playfield for working on.
 	Mat frame;
-	VideoCapture capture(0);
-	if (!capture.isOpened()) {
-		std::cerr << "unable to open camera device. this is fatal - bailing" << std::endl;
-		return(50);
-	}
-    // this throws a SEGFAULT when uncommented. not sure why, but leaving it
-    //    out is harmless - just a waste of a few resources while this runs.
-    //    the camera will be released by the destructor when main returns.
-//	capture.release();
-	// convert captured frame to grayscale and run through theshold to create
-	//    a binary image for higher confidence
-	cvtColor(frame, frame, CV_RGB2GRAY);
-	threshold(frame, frame, 80, 255, CV_THRESH_BINARY);
+//	if (strcmp(argv[1], "--camera") == 0) {
+		VideoCapture capture(0);
+		if (!capture.isOpened()) {
+			std::cerr << "unable to open camera device. this is fatal - bailing" << std::endl;
+			return(50);
+		}
 
-	// create an array of the pre-captured rig images.
-	// verify that all files are extant - fail otherwise
-	//
-	// these images should be binary images. if not, running them
-	//    through threshold is a small matter of adding the code.
+		capture >> frame;
+		cvtColor(frame, frame, CV_RGB2GRAY);
+	//	capture.release();
+/*	} else {
+		std::string filename(argv[1]);
+		struct stat verify;
+		if (stat(filename.c_str(), &verify) == 0) {
+			frame = imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
+		} else {
+			std::cerr << "unable to load frame image. this is fatal - bailing" << std::endl;
+			return(60);
+		}
+	}
+*/	threshold(frame, frame, 80, 255, CV_THRESH_BINARY);
+
 	Mat rigs[4];
-	struct stat filecheck;
-	if ( stat("./rig_square.png", &filecheck) == 0 ) {
+	struct stat verify;
+	if (stat("./rig_square.png", &verify) == 0) {
 		rigs[SQUARE] = imread("./rig_square.png", CV_LOAD_IMAGE_GRAYSCALE);
 	} else {
-		std::cerr << "cannot find rig_square.png\n";
+		std::cerr << "could not locate file 'rig_square.png' - fatal; bailing." << std::endl;
 		return(10);
 	}
-	if ( stat("./rig_square.png", &filecheck) == 0 ) {
+	if (stat("./rig_triangle.png", &verify) == 0) {
 		rigs[TRIANGLE] = imread("./rig_triangle.png", CV_LOAD_IMAGE_GRAYSCALE);
 	} else {
-		std::cerr << "cannot find rig_triangle.png\n";
+		std::cerr << "could not locate file 'rig_triangle.png' - fatal; bailing." << std::endl;
 		return(20);
 	}
-	if ( stat("./rig_square.png", &filecheck) == 0 ) {
+	if (stat("./rig_circle.png", &verify) == 0) {
 		rigs[CIRCLE] = imread("./rig_circle.png", CV_LOAD_IMAGE_GRAYSCALE);
 	} else {
-		std::cerr << "cannot find rig_circle.png\n";
+		std::cerr << "could not locate file 'rig_circle.png' - fatal; bailing." << std::endl;
 		return(30);
 	}
-	// an alternative method identifying the burning rig. this is
-	//    significantly less confident than the one implemented.
-	//    be sure to alter the matchTemplate() call in the for-loop
-	//    below.
-//	Mat flame = imread("rig_flame.png", CV_LOAD_IMAGE_GRAYSCALE);
 
-	// an array of rectangles to create the regions of interest during
-	//    the for-loop. notice that ROI_rigs[NONE] has no shape.
-	Rect ROI_rigs[4] = {
-		Rect(  0,   0,   0,   0),	// here there be MONSTERS
-		Rect( 20, 160, 140, 240),	// SQAURE is in this area
-		Rect(300, 160, 140, 240),	// TRIANGLE is here
-		Rect(480, 160, 140, 240)	// CIRCLES live here
+//	Mat flame = imread("rig_flame.png", CV_LOAD_IMAGE_GRAYSCALE);
+/*	Rect ROI_rigs[4] = {
+		Rect(  0,   0,   0,   0),
+		Rect( 20, 160, 140, 240),
+		Rect(300, 160, 140, 240),
+		Rect(480, 160, 140, 240)
 	};
-	
-	// for us to even consider a confidence value, it MUST be better
-	//    than 1.0 - ignore all higher values.
+*/
+	Rect ROI_rigs[4] = {
+		Rect(  0 ,   0,  0,   0),
+		Rect( 20, 120, 180, 320),
+		Rect(300, 120, 180, 320),
+		Rect(480, 120, 160, 320)
+	};
+
 	double best_confidence = 1.0f;
-	// for each shape, trim the frame down to that shapes pre-determined
-	//    region of interest and then compare that against the pre-
-	//    captured image of a rig.
 	for (short shape = SQUARE; shape <= CIRCLE; shape++) {
-		// crop the frame
 		Mat viewport = frame(ROI_rigs[shape]);
-		// resultant matrix for matchTemplate()
 		Mat result;
 		matchTemplate(viewport, rigs[shape], result, CV_TM_SQDIFF_NORMED);
 	//	matchTemplate(viewport, flame, result, CV_TM_SQDIFF_NORMED);
 
-		// in the resultant matrix, darker values are higher confidence.
-		//
-		// for each shape, find what the highest confidence is (the lowest
-		//    value found in the matrix). if lower than either our limit or
-		//    a previous best_confidence, store this information.
-		double minVal;
+		double minVal, maxVal;
+		Point minLoc, maxLoc;
 		minMaxLoc(result, &minVal);
 		if (minVal < best_confidence) {
 			best_confidence = minVal;
 			matched_rig = shape;
 		}
 
-		// for debugging - can safely ignore
-	//	std::cout << shape << ": " << minVal << std::endl;
+		std::cout << shape << ": " << minVal << std::endl;
+
+		// display both viewport and result to user, wait to continue
+		namedWindow("viewport", CV_WINDOW_AUTOSIZE);
+		imshow("viewport", viewport);
+		namedWindow("result", CV_WINDOW_AUTOSIZE);
+		imshow("result", result);
+		while(waitKey() != 27);
+		destroyAllWindows();
+	//*/
 	}
 
-	// for debugging - can safely ignore
 //	std::cout << "best confidence: " << best_confidence << std::endl;
-
-	// during testing using the array of pre-captured images, it was
-	//    found that a confidence found of less than .0500 was good
-	//    enough to determine the burning rig. if you desire a safer
-	//    value (due to light constraints), .1000 is also a good value.
-	if (best_confidence < 0.050000) {
-		// this output block allows the user to see what was found
+	// or .1000 if you want to be safe
+	if (best_confidence < 0.075000) {
 		std::cout << "WESLEY :: id_flame --> I see a ";
 		switch(matched_rig) {
 			case SQUARE:
@@ -180,10 +179,7 @@ int main(int argc, char* argv[]) {
 		}
 		std::cout << std::endl;
 	} else {
-		// if the confidence was not within our tolerance, reset
-		//    matched_rig to NONE and continue. 
 		matched_rig == NONE;
 	}
-	// return matched_rig back to caller.
 	return(matched_rig);
 }
