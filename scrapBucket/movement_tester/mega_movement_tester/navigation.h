15e14c3e74435f01632fedccc37c88ab35c2f15c
vptarmigan <vicky.ptarmigan@gmail.com>
2014-03-01 14:59:56 -0600

    added traveling + turn towards lane

--- scrapBucket/movement_tester/mega_movement_tester/navigation.h
+++ scrapBucket/movement_tester/mega_movement_tester/navigation.h
@@ -39,6 +39,10 @@ class Navigation {
                 GapFinder gapfind;
                 QuadEncoder encoders;
                 ParallelPark par; 
+                Magellan mag;
+                
+                const static int32_t ticksFor90 = 3450;
+                bool goingForward;
     public:
                 void init()  {
                   sabertooth.begin(2);        //motor controller on serial2
@@ -46,12 +50,19 @@ class Navigation {
                   mov.init(&sabertooth);
                   Serial.println("ready");
                   gapfind.init(A0,A1,A2);
-                  eyes.init(A7,15);
+                  eyes.init(A3,15);
+                  mag.init(A6,A7);
                   par.init(A2,A1,A0);
+                  
+                  goingForward = true;  //... this is from the point of view of robot. it will always start going forward.
+                                        //it's just that the motor library has it as "reverse()" as our going forward XD
                 }
                 
                 
                 //Stuff that needs to be updated goes in here.
+                //NO NO NO NO NONONONONONONO
+                //update-y things need to GO IN THE THING ITSELF BLAHHHHHHHHH or be called in the nav functions themselves nonono
+                /*
                 void update()  {
                 //keep going forward until you find gap
                     if(!gapfind.gapPresent())
@@ -65,9 +76,52 @@ class Navigation {
                     //   sabertooth.all_stop();
                 
                 } 
+                */
                 
                 void takeOff()  {
                   sabertooth.reverse(20);
+                  goingForward = true;
+                }
+                
+                //toggle in forward or backwards when magellans see seomthing
+                void traveling()  {
+                   mag.printDebug();
+                   //mag.printDebugDifference();
+                   mag.printEdgeStatus();
+                   mag.update();
+                   //if it senses ANYTHING reverse direction quick!
+                  /*
+                  //twitches and just twitches forward and backward
+                   if( !mag.isFrontSafe() || !mag.isBackSafe())  {
+                     Serial.println("ACK SOMETHING!");
+                     sabertooth.all_stop();
+                     delay(500);
+                     goingForward = !goingForward;
+                   } 
+                   */
+                   
+                   if(!mag.isFrontSafe() && goingForward)  {
+                     Serial.println("ACK SOMETHING!");
+                     sabertooth.all_stop();
+                     delay(500);
+                     goingForward = false;
+                     
+                  
+                   }
+                   if(!mag.isBackSafe() && !goingForward)  {
+                     Serial.println("ACK SOMETHING!");
+                     sabertooth.all_stop();
+                     delay(500);
+                     goingForward = true;
+                   }
+                   if(goingForward)  {
+                     Serial.println("go forward");
+                     sabertooth.reverse(20);
+                   }
+                   else  {
+                     Serial.println("go reverse");
+                     sabertooth.forward(20);
+                   }
                 }
                 
                 //returns if gap is found.
@@ -78,6 +132,9 @@ class Navigation {
                   //check if a gap has been found
                   gapfind.printDebug();
                   gapfind.printGapStatus();
+                  gapfind.update();
+                  
+                  
                   //gap found? move forward a set amount to center self
                   if(gapfind.gapPresent())  {
                     //Serial.println("moving \t GAP FOUND!!");
@@ -94,14 +151,22 @@ class Navigation {
                   return false;
                 }
                 
+                void turnTowardsLane()  {
+                  mov.turn90degreesCW(0x60,0x20,ticksFor90);
+        
+                  sabertooth.all_stop();
+                  delay(500);
+                }
+                
                 void turnTowardsGap()  {
-                  mov.turn90degreesCW(0x20,0x60);
+                  mov.turn90degreesCW(0x20,0x60,ticksFor90);
         
                   sabertooth.all_stop();
                   delay(500);
                 }
                 
                 bool crossGap()  {
+                  
                   sabertooth.reverse(20);
                    eyes.update();
                    eyes.printDebug();
@@ -114,13 +179,7 @@ class Navigation {
                 }
                 
                 void parallelpark()  {
-                 /*
-                  while(true)    { 
-                   par.printDebug(); 
-                   par.update();
-                   //par.printStatus();
-                 }
-                 */
+                
                   
                   //WHOAAAAH
                   //change of paradigm.
@@ -130,27 +189,13 @@ class Navigation {
                   while(!par.isParallel())  {
                     mov.turn(0x60,0x20);
                     //keep going
-                    par.printDebug();
+                    //par.printDebug();
                     par.printStatus();
                     par.update();
                   }
-                  
-                  /*
                   sabertooth.all_stop();
-                  
-                  while(par.needToTurnCW())  {
-                     mov.turn(0x20,0x60);
-                     par.printDebug();
-                     par.printStatus();
-                     par.update();
-                     delay(100);
-                     sabertooth.all_stop(); 
-                  }
-                  */
-                  
-                  //sabertooth.all_stop();
-                  
-                  //jump out when it is parallel hopefully
+               
+       
                 }
                 void sleep()  {
                   sabertooth.all_stop();
