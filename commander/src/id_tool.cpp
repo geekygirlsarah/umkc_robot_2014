328c2ff71373d9212ce648c8a3cb8482a260b5f5
Chris Denniston <octopuscabbage@gmail.com>
2014-03-08 17:11:35 -0600

    Began work on id_tool submodule

--- commander/src/id_tool.cpp
+++ commander/src/id_tool.cpp
@@ -1,4 +1,20 @@
 #include "id_tool.h"
+#include <unistd.h>
+#include <sys/types.h> /* for pid_t */
+#include <sys/wait.h> /* for wait */
+using namespace std;
 
 int id_tool::execute(){
-	exec(
+	 /*Spawn a child to run the program.*/
+    pid_t pid=fork();
+    if (pid==0) { /* child process */
+        static char *argv[]={"echo","Foo is my name.",NULL};
+        exec("/bin/id_tool");
+        exit(127); /* only if execv fails */
+    }
+    else { /* pid!=0; parent process */
+        waitpid(pid,0,0); /* wait for child to exit */
+    }
+    return 0;
+}
+
