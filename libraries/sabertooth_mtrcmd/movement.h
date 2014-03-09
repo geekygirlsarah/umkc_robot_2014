41ddf1090e08018d1bd462c1bdb867a4060db8d0
vptarmigan <vicky.ptarmigan@gmail.com>
2014-03-08 22:06:40 -0600

    ticks as a param to turn

--- libraries/sabertooth_mtrcmd/movement.h
+++ libraries/sabertooth_mtrcmd/movement.h
@@ -25,18 +25,23 @@ class movement {
                   saber = s;
                 }
 
+				//0x60 | 0x20 = CCW
+				//0
+
 				//perpetually turn  CW
+				/*
 				void turn(byte right, byte left)	{
 					Serial.println("movement::forever turn");
 					saber->rightMotorCommand(right);
 					saber->leftMotorCommand(left);
 				
 				}
+				*/
 		//right = 0x50, left = 0x30 for clock wise
 		//turn 90 degrees... in 10 degree increments in order to combat the slip
-		void turn90degreesCW(byte right, byte left)  {
+		void turn90degreesCW(byte right, byte left, int32_t ticks)  {
 			//need to find out current ticks
-			int32_t ticksFor90 = 3540;  //ticks that right or left side to turn 90 degrees, if both right and left turn in opp directions
+			//int32_t ticks = 3540;  //ticks that right or left side to turn 90 degrees, if both right and left turn in opp directions
 			int32_t rightTicks_start = getCurrentRightTicks();
 			int32_t leftTicks_start = getCurrentLeftTicks();
 			
@@ -72,7 +77,7 @@ class movement {
 			  Serial.print(average_interval); 
 			  Serial.println();    
 			  
-			  if(average_interval > (ticksFor90))  {
+			  if(average_interval > (ticks))  {
 				break;
 			  }
 			}
