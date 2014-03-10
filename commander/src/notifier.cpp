f289b004e74092db4cfaf7de476df7ca19a160f9
octopuscabbage <octopuscabbage@gail.com>
2014-03-09 19:57:22 -0500

    Changed the format of the id file to use integers instead of booleans because apparently that was undefined behavior.

--- commander/src/notifier.cpp
+++ commander/src/notifier.cpp
@@ -12,13 +12,20 @@ void LedNotifier::parse(const char* parseFileName){
 	}
 	while(fin.good()){
 		string name;
-		bool green0;
-		bool green1;
-		bool yellow0;
-		bool yellow1;
-		bool red0;
-		bool red1;
-		fin >> name >> green0>>green1>>yellow0 >> yellow1 >> red0 >> red1;
+		int green0in;
+		int green1in;
+		int yellow0in;
+		int yellow1in;
+		int red0in;
+		int red1in;
+		fin >> name >> green0in>>green1in>>yellow0in >> yellow1in >> red0in >> red1in;
+		//Stupid way to convert these two booleans but yolo
+		bool green0 = (green0in == 1);
+		bool green1 = (green1in == 1);
+		bool yellow0 = (yellow0 == 1);
+		bool yellow1 = (yellow1 == 1);
+		bool red0 = (red0in == 1);
+		bool red1 = (red1in == 1);
 		notificationMap[name] = LedArray(green0,green1,yellow0,yellow1,red0,red1);
 	}
 }	
