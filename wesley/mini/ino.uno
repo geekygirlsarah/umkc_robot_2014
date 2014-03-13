8fa9cd68b61e2fdf0ac5f2aee0c0b628b02ff3fe
Eric M Gonzalez <eric.gonzalez@umkc.edu>
2014-03-13 15:53:39 -0500

    preliminary button / LED code
    Added to the wesley/mini/ directory is code to handle the topics:
    
    	/master/button - a published topic that indicates when the go button
    						has been pushed on the robot.
    	/master/leds   - a subscribed topic to set the LEDs in the satus
    						display to specific pattern. Currently this is
    						in the form of:
    							geometry_msgs/Point {
    								int32 x
    								int32 y
    								int32 z
    							}
    
    Included in this commit is the rosserial_arduino library, located here:
    
    	wesley/mini/lib/ros_lib
    
    this folder can be copied as is to your ~/sketchbook/libraries folder
    and used by including <ros.h>. please see:
    
    	http://wiki.ros.org/rosserial_arduino/Tutorials
    
    for more information on using the rosserial_arduino library.

---
+++ wesley/mini/ino.uno
@@ -0,0 +1,9 @@
+[build]
+board-model = uno
+
+[upload]
+board-model = uno
+serial-port = /dev/arduino_uno
+
+[serial]
+serial-port = /dev/arduino_uno
--- wesley/mini/src/sketch.ino
+++ wesley/mini/src/sketch.ino
@@ -1,54 +1,106 @@
-/* WESLEY_SKETCH_MINI
- * written by: Eric M Gonzalez
- * date: 08-01-14
- *
- * PURPOSE: This sketch contains the response side of IR_CLASS_MEGA and
- *          resides on the mini.
- *
- *          REMEMBER TO MAKE SURE THAT THE SERIAL LINE-SPEED USED HERE
- *          MATCHES WHAT IS USED IN IR_CLASS_MEGA!
- */
-#include "wesley_mini_bridge.h"
-mini_bridge mega_br;
+#include <ros.h>					// basic ROS objects
+#include <geometry_msgs/Point.h>	// a pre-generated 3 number message
+#include <std_msgs/Bool.h>			// button: true / false
 
-#include "gapFinder.h"
-GapFinder gap_state;
-// from mini-->mega
+// majority of code lives in the ROS namespace
+using namespace ros;
 
-byte no_of_sensors;
+// set pins to easy-to-read names
+const byte BTN =  7;
+const byte RED =  8;
+const byte YLW =  9;
+const byte GRN = 10;
 
-void setup() {
-	no_of_sensors = 3;
-//	gap_state.init(A0, A1, A2);
-	mega_br.begin(no_of_sensors);
+// subscriber call back. as soon as mini has latched into ROS, this topic
+//    is available to be written to. receive a 3 number message and set
+//    the status of individual pins based on what the status is.
+//
+// WESLEY has a 6 LED status block, and so we'll need a custom message.
+//    or, just a byte message and then we can flag out the bits to find
+//    out which LEDs need lit.
+void display_status(const geometry_msgs::Point& status) {
+	digitalWrite(RED, status.x);
+	digitalWrite(YLW, status.y);
+	digitalWrite(GRN, status.z);
 }
 
-void loop() {
-	/* commented off for testing;
-	if (gap_state.findGap() == gap_state.MAYBE_GAP) {
-		mega_br.speed(mega_br.HALF_SPEED);
-	} else
-	if (gap_state.findGap() == gap_state.YES_GAP) {
-		mega_br.speed(mega_br.FULL_STOP);
-	} else
-	if (mega_br.cmd_waiting()) {
-		byte cmd = mega_br.cmd();
-		switch(cmd) {
-			case '$': {
-				// package and send off an array of cm distances
-				gap_state.report(mega_br.data_packet);
-				mega_br.tunnel();
-				break;
-			}
-			default: {
-				break;
-			}
-		}
-	}	*/
+// create the node handle for this arduino.
+NodeHandle nh;
 
-	delay(random(500, 1400));
-	mega_br.speed(random(0,30));
-//	Serial.print("mini: ");
-//	Serial.print(random(0, 50), DEC);
-//	Serial.println();
+// keep track of running_state : this is altered by a button press.
+std_msgs::Bool running_state;
+
+Publisher                        pub("/master/button", &running_state);
+Subscriber<geometry_msgs::Point> sub("/master/leds",   &display_status);
+
+void setup() {
+	// status display LEDs
+	pinMode(RED, OUTPUT);
+	pinMode(YLW, OUTPUT);
+	pinMode(GRN, OUTPUT);
+	{
+		digitalWrite(RED, HIGH);
+		digitalWrite(YLW, LOW);
+		digitalWrite(GRN, LOW);
+	}
+
+	// button and pull-up resistor
+	pinMode(BTN, INPUT);
+	digitalWrite(BTN, HIGH);
+
+	// through experiments, 19200 was discovered to be the slowest
+	//    baud-rate that allowed this all to work. never sure why.
+	nh.getHardware()->setBaud(19200);
+
+	// initialize the node and attach the publication and subscription
+	nh.initNode();
+	nh.advertise(pub);
+	nh.subscribe(sub);
+
+	// set initial values of check.
+	running_state.data = false;
+	{
+		delay(200);
+		digitalWrite(RED, LOW);
+		digitalWrite(YLW, HIGH);
+		digitalWrite(GRN, LOW);
+	}
+}
+
+// a check to see if we've published our message yet.
+// this should be doable with only one boolean. my
+//    logic is off somwhere.
+//
+// correctness now, completeness later.
+bool published = false;
+
+// main loop.
+void loop() {
+	// 1) if we haven't published our message
+	if (!published) {
+	// 2) if the previous button state was false
+		if (running_state.data == false) {
+	// 3) read the button again
+			running_state.data = !digitalRead(BTN);
+	// 4) else, if the previous button state was true
+		} else {
+	// 5) publish the state of our button
+			pub.publish(&running_state);
+			{
+	// 6) TEMPORARY - the LEDs would be set by another node
+				digitalWrite(RED, LOW);
+				digitalWrite(YLW, LOW);
+				digitalWrite(GRN, HIGH);
+			}
+	// 7) set master boolean so we never check this segment again.
+			published = true;
+		}
+	}
+
+	// 8) sit and spin, checking each round for:
+	//    a) a message in on /master/leds
+	//    b) or a message waiting to go out on /master/button
+	nh.spinOnce();
+	delay(5);
+
 }
