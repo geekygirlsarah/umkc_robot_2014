dd6d9ee18d156c9052c9ddedf7b0b3d91592df39
Victoria Wu <victoria.chen.wu@gmail.com>
2014-03-14 01:03:47 -0400

    mega_caretaker bare package up + cmake

---
+++ ros_sandbox/src/mega_caretaker/include/mega_caretaker/mega_caretaker.h
@@ -0,0 +1,27 @@
+#ifndef MEGA_CARETAKER_H 
+#define MEGA_CARETAKER_H 
+
+#include "ros/ros.h"
+#include "ros/package.h"
+#include "std_msgs/String.h"
+
+//msg includes
+//#include "mega_caretaker/Mega.h"
+
+
+
+namespace mega_gatekeeper	{
+
+		class MegaGatekeeper	{
+			private:
+					
+					ros::NodeHandle node;
+
+			public:
+					void init(ros::NodeHandle n);
+		};
+
+
+}
+
+#endif
---
+++ ros_sandbox/src/mega_caretaker/src/mega_caretaker.cpp
@@ -0,0 +1,20 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+#include <sstream>
+
+#include "mega_caretaker/mega_caretaker.h"
+
+//using namespace mega_gatekeeper;
+
+//void MegaGatekeeper::init(ros::NodeHandle n)	{
+//	node = n;
+//}
+
+int main(int argc, char** argv)	{
+	ros::init(argc, argv, "mega_gatekeeper");
+	ros::NodeHandle n;
+//	MegaGatekeeper m;
+//	m.init(n);
+	//m.run();
+}
