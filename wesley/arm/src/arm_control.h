cb9d241ac123b969b9654339ce9c6ff3553d053e
Sarah Withee <sarah@sarahwithee.com>
2014-03-22 15:14:25 -0500

    Refactored functions so that servo writes can be limited easier. Replaced put/p_put with putAngle/putPulse. Merged Eric's forward kinematics and update() updates to my files.

--- wesley/arm/src/arm_control.h
+++ wesley/arm/src/arm_control.h
@@ -39,56 +39,35 @@ class arm_control {
 		short* p_position;
 
 	public:
-        	enum JOINTS { BASE, SHOULDER, ELBOW, WRIST_P, WRIST_R, HAND };
-        	struct point {
-        		int x, y, z;
-        		point(int x = 0, int y = 0, int z = 0) :
-        			x(x), y(y), z(z) { };
-        	};
+        enum JOINTS { BASE, SHOULDER, ELBOW, WRIST_P, WRIST_R, HAND };
+        struct point {
+    		int x, y, z;
+    		point(int x = 0, int y = 0, int z = 0) :
+    			x(x), y(y), z(z) { };
+    	};
 
-        // Constructor
+        // Constructor/deconstructor
 		arm_control();
-
-        // Parameterized constructor
 		arm_control(byte joints);
-
-        // Deconstructor
 		~arm_control();
 
-        // Attaches a set of pins to the servos
+        // Initializing functions
 		void connect(byte argc, ...);
-
-        // This needs to be called immediately after connect.
-		// this sets the arm into a sane, locked position.
-		// these values can be adjusted as necessary.
+		byte read(const byte joint);
 		void initial_park();			
 
-        // Puts the arm in a good resting (parked) position
+        // Exact placement functions
 		void park();
-
-        // This puts the arm in a good position to carry a tool as the bot 
-		// drives around. Wrist will point up.
 		void carry();
 
-        // Returns the angle value of the servo
-		byte read(const byte joint);
+        // Exact movement functions
+		void putPulse(const byte joint, const short pulse);
+		void putAngle(const byte joint, const byte angle);
+    	void update();
 
-        // Directly write a pulse to the servo, would take the place of the 
-		// function put (byte, byte)         
-		void p_put(const byte joint, short pusle);
-
-        // Directly put the given joint to the given angle,
-		// converting first to pulse width
-		void put(const byte joint, const byte angle);
-
-        // Call update with update(NO_OF_JOINTS, <a list of joints to mov
-    	void update(const byte argc, ...);
-        
-        // Places at (x, y, z) - this is an inverse kinematic
-		//    equation that translates the (x, y, z) into
-		//    angualr measures from an origin defined at the
-		//    base of the arm.
-		void move_to( float x, float y, float z, float grip_angle_d, bool moveSmooth = true);
+        // Relative movement functions
+		void move_to( float x, float y, float z, float grip_wrist, float grip_rotate);
+        struct point getXYZ() {
 };
 
 // EOF
