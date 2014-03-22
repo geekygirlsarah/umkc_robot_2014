1f25d8e7a4794f7c40b7103f4a0117ceb085aa64
UMKC Robotics <umkc@robodevel3.kc.umkc.edu>
2014-03-22 15:24:25 -0500

    added state info

--- ros_sandbox/src/mega_caretaker/include/mega_caretaker/redux_mega_packet_defs.h
+++ ros_sandbox/src/mega_caretaker/include/mega_caretaker/redux_mega_packet_defs.h
@@ -12,7 +12,14 @@
 	const static int8_t PL_FINISHED_WAVE_CROSSING = PL_START_WAVE_CROSSING;
 	const static int8_t PL_FINISHED_TURNING_90 = PL_START_TURNING_90;
 
-	const static int8_t MSGTYPE_STATE = 1;
+	const static int8_t MSGTYPE_STATE = 2;
+	const static int8_t PL_WAITING = 0;
+	const static int8_t PL_START = 1;
+	const static int8_t PL_TURNING_CW_INIT = 2;
+	const static int8_t PL_TURNING_CW_WAIT = 3;
+	const static int8_t PL_TURNING_CW_FIN = 4;
+	const static int8_t PL_END = 5;
+
 
 	const static int8_t MSGTYPE_MOTORCOM = 3;
 	const static int8_t PL_STOP = 0;
