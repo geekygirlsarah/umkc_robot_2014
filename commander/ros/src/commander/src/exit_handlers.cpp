76c6613f390276cee4100bebb5a8a99fd0492980
Chris Denniston <octopuscabbage@gmail.com>
2014-03-22 15:23:29 -0500

    Probably ready to merge now?

--- commander/ros/src/commander/src/exit_handlers.cpp
+++ commander/ros/src/commander/src/exit_handlers.cpp
@@ -12,46 +12,56 @@ void ExitHandler::id_flame(int returnCode){
 	switch(returnCode)
 	{
 	case 0:
-	{
+	
 		ledNotifier.throwLedCode("id_flame_notfound");
-	}
+		break;
+	
 	case 1:
-	{
+	
 		flame = 1; 
 		ledNotifier.throwLedCode("id_flame_square");
-	}
+		break;
+	
 	case 2:
-	{
+	
 		flame = 2;
 		ledNotifier.throwLedCode("id_flame_triangle");
-	}
+		break;
+	
 	case 3:
-	{
+	
 		flame = 3;
 		ledNotifier.throwLedCode("id_flame_circle");
-	}
+		break;
+	
 	case 10:
 	case 20:
 	case 30:
 	case 40:
-	{
+	
 		ledNotifier.throwLedCode("cant_find_file");
-	}
-}
-	//set our internal private variable flame = to returnCode
-	//for future reference in other programs
-	flame = returnCode;
+		break;
+	
+	case -1:
+		ledNotifier.throwLedCode("general_failure");
+		break;
 }
 
 //id_tool function will take our return code from id_tool program and 
 //throw led notification based on the outcome
 void ExitHandler::id_tool(int returnCode)
 {
-	if(returnCode == 0)
-	{
-		ledNotifier.throwLedCode("id_tool_failure");
+	switch(returnCode){
+		case 0:
+			ledNotifier.throwLedCode("id_tool_failure");
+			break;
+		case -1:
+			ledNotifier.throwLedCode("general_failure");
+			break;
+		default:	
+			//set our private variable tool = to returnCode for documentation
+			tool = returnCode;
+			break;
 	}
-	//set our private variable tool = to returnCode for documentation
-	tool = returnCode;
 }
 
