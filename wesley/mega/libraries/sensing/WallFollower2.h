73e9334546320332092031cc7615260f24786f78
Chris Denniston <octopuscabbage@gmail.com>
2014-03-27 20:46:41 -0500

    Added class WallFollow in WallFollwer2.h
    WallFollow uses algorithms and new speeds calculated to make sure the robot
    continues on a straight path
    //
    //TODO: Test and make sure all the function co-exist with other utilization
    //of motors
    //

---
+++ wesley/mega/libraries/sensing/WallFollower2.h
@@ -0,0 +1,202 @@
+//WALL FOLLOWER V2//
+
+
+// CHASE PETERSON //
+
+#include <DistSmoother.h>
+#include <motor_cmd.h>
+#include <stdlib.h>
+
+#ifndef WALLFOLLOWER2_H
+#definite WALLFOLLOWER2_H
+
+class WallFollow{
+
+private:
+	motor_cmd* saber;
+	enum internal  {good, list_right, list_left};
+	internal straightness;
+	bool straight;
+	DistSmoother front, middle, back;
+	bool notGood;
+	int UPPER_LIMIT, LOWER_LIMIT, MIDDLE;
+	
+public:
+
+	void setLimits(int far, int mid, int close)
+	{
+		UPPER_LIMIT = far;
+		MIDDLE = mid;
+		LOWER_LIMIT = close;
+	}
+	
+	void init(int pin1 , int pin2, int pin3, motor_cmd*  s) 
+	{
+		front.init(pin1);
+		middle.init(pin2);
+		back.init(pin3);
+		saber = s;
+		straightness = good;
+		stright = true;	
+		notGood = false;
+	}
+	
+	internal straight()
+		{
+		internal result;
+		notGood = false;
+		if(front.getAccurateDistCM() > 25)
+			{
+				
+				if(middle.getAccurateDistCM() > UPPER_LIMIT)
+				{
+					notGood = true;
+					Serial.println("TOO FAR AWAY");
+					return list_right;
+				}
+				else if(middle.getAccurateDistCM() < LOWER_LIMIT)
+				{
+      
+					notGood = true;
+					Serial.println("TOO CLOSE");
+					return list_left;
+				}
+				
+			}
+			
+		else if(middle.getAccurateDistCM() > 25)
+			{
+				if(front.getAccurateDistCM() > UPPER_LIMIT)
+				{
+					notGood = true;
+					Serial.println("TOO FAR AWAY");
+					return list_right;
+				}
+     
+				else if(front.getAccurateDistCM() < LOWER_LIMIT)
+				{
+					notGood = true;
+					Serial.println("TOO CLOSE");
+					return list_left;
+				}
+			}
+		else if(back.getAccurateDistCM() > 25)
+			{
+				if(front.getAccurateDistCM() > UPPER_LIMIT)
+				{
+					notGood = true;
+					Serial.println("TOO FAR AWAY");
+					return list_right;
+				}
+				else if(front.getAccurateDistCM() < LOWER_LIMIT)
+				{
+					notGood = true;
+					Serial.println("TOO CLOSE");
+					return list_left;
+				}
+			}	
+  
+	
+		else
+			{
+				if(IR_F.getAccurateDistCM() > UPPER_LIMIT)
+				{
+					notGood = true;
+					Serial.println("TOO FAR AWAY");
+					return list_right;
+				}
+				else if(IR_F.getAccurateDistCM() < LOWER_LIMIT)
+				{
+					notGood = true;
+					Serial.println("TOO CLOSE");
+					return list_left;
+				}
+			}
+		return result;
+		}
+		
+	
+	internal update(const byte DIRECTION)
+	{
+		int outerloopcount = 1;
+		while(outerloopcount-- > 0)
+		{
+			switch(straightness)
+			{
+				case good:
+					if(straight() == list_left)
+						{
+						straightness = list_left;
+						if(DIRECTION == S->REVERSE)
+							S.reverse_correct_right();
+						else if(DIRECTION == S->FORWARD)
+							S.forward_correct_right();
+						}
+					else if(straight() == list_right)
+						{
+						straightness = list_right;
+						if(DIRECTION == S->REVERSE)
+							S.reverse_correct_left();
+						else if(DIRECTION == S->FORWARD)
+							S.forward_correct_left();
+						}
+					else
+						{
+						if(DIRECTION == S->REVERSE)
+							S.reverse();
+						else if(DIRECTION == S->FORWARD)
+							S.forward();
+						}
+				case list_left:
+					if(straight() == good)
+						{
+						straightness = good;
+						if(DIRECTION == S->REVERSE)
+							S.reverse();
+						else if(DIRECTION == S->FORWARD)
+							S.forward();
+						}
+					else if(straight() == list_right)
+						{
+						straightness = list_right;
+						if(DIRECTION == S->REVERSE)
+							S.reverse_correct_left();
+						else if(DIRECTION == S->FORWARD)
+							S.forward_correct_left();
+						}
+					else
+						{
+						straightness = list_left;
+						if(DIRECTION == S->REVERSE)
+							S.reverse_correct_right();
+						else if(DIRECTION == S->FORWARD)
+							S.forward_correct_right();
+						}
+				case list_right:
+					if(straight() == good)
+						{
+						straightness = good;
+						if(DIRECTION == S->REVERSE)
+							S.reverse();
+						else if(DIRECTION == S->FORWARD)
+							S.forward();
+						}
+					if(straight() == list_left)
+						{
+						straightness = list_left;
+						if(DIRECTION == S->REVERSE)
+							S.reverse_correct_right();
+						else if(DIRECTION == S->FORWARD)
+							S.forward_correct_right();
+						}
+					else 
+						{
+						straightness = list_right;
+						if(DIRECTION == S->REVERSE)
+							S.reverse_correct_left();
+						else if(DIRECTION == S->FORWARD)
+							S.forward_correct_left();
+						}
+				return straightness;
+			}
+};
