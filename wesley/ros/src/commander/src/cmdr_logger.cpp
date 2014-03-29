4de889cca70c05f89d43b19470279af186610b70
Eric M Gonzalez <eric.gonzalez@umkc.edu>
2014-03-29 04:19:58 -0500

    corrected cmdr_logger filename format.
    you may call cmdr_logger with or with out providing
       a path on the commandline. path only; must exist.
    
    ex)
    	rosrun commander cmdr_logger
    or
    	rosrun commander cmdr_logger /home/temp/logs
    
    do *not put a trailing slash on any provided paths.

--- wesley/ros/src/commander/src/cmdr_logger.cpp
+++ wesley/ros/src/commander/src/cmdr_logger.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <ctime>
 #include <string>
+#include <sstream>
 using std::string;
 
 std::ofstream fout;
@@ -28,11 +29,25 @@ int main(int argc, char **argv)
 	timeinfo = localtime(&date);
 	
 	
+	string prefix = "/home/umkc/wesley/logs";
+	if (argc == 2) {	// this means that someone provided 
+						//    a path prefix for the log file.
+		prefix = argv[1];
+	}
+
+	std::stringstream ss;
 	// filename has format: 
 	//   DATE-TIME.log
 	//   2014-03-14_1502.log
-	string extension = ".log";
-	string filename = asctime(timeinfo) + extension;
+	//
+	// i don't make this up.. read 'man 3 localtime' and see how the struct is set up.
+	ss << prefix << "/" <<   timeinfo->tm_year  + 1900
+				 << "-" << ((timeinfo->tm_mon + 1 < 10) ? "0" : "") << timeinfo->tm_mon + 1
+				 << "-" <<   timeinfo->tm_mday
+				 << "_" << ((timeinfo->tm_hour    < 10) ? "0" : "") << timeinfo->tm_hour
+						<< ((timeinfo->tm_min     < 10) ? "0" : "") << timeinfo->tm_min
+						<< ".log";
+	string filename = ss.str();
 	fout.open (filename.c_str());
 	
 	ros::init(argc,argv,"speaker_for_the_dead");
