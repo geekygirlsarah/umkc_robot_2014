df3c54c55d64f6a0a673a8d561cf34c60a3acc55
Sarah Withee <sarah@sarahwithee.com>
2014-03-29 23:43:29 -0500

    Revert "My work in progress..."
    This reverts commit d7b5a6c916ce48732dabb2fd0db29ac39de79f77.

--- wesley/mega/src/src.ino
+++ wesley/mega/src/src.ino
@@ -82,7 +82,6 @@ bool crossBoard;
 bool  commandGoToTools;
 bool  isGapFound;
 bool  isGapCrossed;
-bool restartGapFound;
 bool  isEdgeFound;
 bool travelingHome;
 
@@ -579,7 +578,6 @@ void setup() {
   isGapFound = false;
   isGapCrossed = false;
   isEdgeFound = false;
-  restartGapFound = false;
   travelingHome = false;
   
   #ifdef DEBUG_COMMS
@@ -711,14 +709,8 @@ void loop() {
      }
      */
    #endif
-   
-   // Double-check that we're at a gap first before turning
-   //  ???   if(gapfinder.gapPresent()) {
-      stateMachine.transitionTo(turn90Degrees_CCW);
-    // }
-    // else {
-    //  
-    //}     
+     stateMachine.transitionTo(turn90Degrees_CCW);
+     
    
    }
    
