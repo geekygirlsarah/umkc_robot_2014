0b4907969062a7681a663f44aadabd22096957f0
octopuscabbage <octopuscabbage@gmail.com>
2014-04-02 01:51:49 -0500

    Finished watchdog

--- wesley/ros/src/commander/src/watchdog.h
+++ wesley/ros/src/commander/src/watchdog.h
@@ -1,15 +1,33 @@
 #include <string>
 #include <unistd.h>
 #include <signal.h>
+#include <thread>
+#include <map>
+#include <stdio.h>
+using std::map;
 using std::string;
 
+#ifndef WATCHDOG_H
+#define WATCHDOG_H
+/**
+ * Watch dog is a functor object that should be called as such, don't worry it suicides itself and deallocates it's own memory
+ * WatchDog* watch = new WatchDog; 
+ * std::thread(watch(binaryName));
+ */
 class WatchDog{
-	int timesCalled;
-	void callback(string binaryName)
+	string name;
+	int amountCalled;
+	int checkAmount; /*<The time in near-seconds for the thread to be alive before it's killed */
+	const int seconds_to_sleep = 1;
+	//Tick through each process updating the amount of times it's been called in call_map and kills it if it's been checked checkAmount times
+	void tick();
 	void execute_pkill(string name);	
+	 
 public:
-	WatchDog(){timesCalled = 0;}
-	WatchDog(int times){timesCalled=times;};
-	void operator()(string binaryName){
-		signal(SIGALARM,callback(binaryName);
+	WatchDog(){checkAmount=1000;}
+	WatchDog(int amountOfTicksBeforeKilled){checkAmount=amountOfTicksBeforeKilled;}
+	void operator()(string binaryName);
+
+};
+#endif
 
